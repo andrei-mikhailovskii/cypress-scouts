23cd52bedf78450f8b0ed4f78286a1556d881d49
andrei-mikhailovskii <dr.mikhaylovsky@gmail.com>
2023-06-12 19:27:39 +0200

    replaced productTitle variable with alias

--- cypress/e2e/addProductToCart.cy.js
+++ cypress/e2e/addProductToCart.cy.js
@@ -3,9 +3,6 @@ Cypress._.times(10, () => {
 describe('Add product to cart', () => {
   it('Checks if product added to cart', () => {
 
-    //variable for future comparison of values
-    let productTitle;
-
     // open website
     cy.visit('/');
 
@@ -13,8 +10,9 @@ describe('Add product to cart', () => {
     cy.get('h4.card-title')
       .first()
       .invoke('text')
-      .then((firstProductName) => {
-        productTitle = firstProductName
+      .as('productTitle')
+      .then((productTitle) => {
+        cy.wrap(productTitle).as('productTitleValue');
       });
 
     // click first item in grid
@@ -27,13 +25,11 @@ describe('Add product to cart', () => {
     cy.contains('#cartur', 'Cart').click();
 
     // verify if item is in the cart
-    cy.get('.success > td')
-      .eq(1)
-      .should('be.visible', { timeout: 5000 })
-      .invoke('text')
-      .then((cartItemName) => {
-        expect(cartItemName).to.equal(productTitle)
+    cy.get('@productTitleValue').then((productTitleValue) => {
+      cy.get('.success > td').eq(1).invoke('text').then((cartItemName) => {
+        expect(productTitleValue).to.be.eq(cartItemName);
       });
+    });
       
   })
 })
